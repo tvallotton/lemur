bd570d7ce1969bbda56d12b8349781ad4d7fba03
Tomas Vallotton <Tomas@MacBookPro-2.local>
2020-12-24 01:32:17 -0300

    -

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -23,57 +23,6 @@ impl<'a> Parser<'a> {
     }
 }
 
-// impl<'a> Parser<'a> {
-//     fn build_next(&mut self) -> Result<AST, SyntaxError> {
-//         let option = self.lexer.next();
-
-//         if let Some(result) = option {
-//             let token = result?;
-
-//             if token == Token::Keyword("module") {
-//                 self.handle_module();
-//             }
-//             // else if token == Token::Keyword("import") {
-//             //     self.handle_import();
-//             // }
-//             // else if token == Token::Keyword("export") {
-//             //     self.handle_import();
-//             // }
-//             // else if token == Token::Keyword("data") {
-//             //     self.handle_data();
-//             // }
-//             // else if token == Token::Keyword("enum") {
-//             //     self.handle_enum();
-//             // }
-//             // else if token == Token::Keyword("instance") {
-//             //     self.handle_instance();
-//             // }
-//             // else if token == Token::Keyword("class") {
-//             //     self.handle_class();
-//             // }
-//             // else if token == Token::Keyword("type") {
-//             //     self.handle_type()
-//             // }
-//             else if let token = Token::Variable(name) {
-//                 self.handle_variable(name)
-//             }
-//         }
-//         else {
-//             None
-//         }?;
-//     }
-
-//     fn read_variable(&mut self) ->  {
-//         if let Some(Ok(Token::Variable(name))) = self.lexer.next() {
-
-//             Ok(ast::Expr::Primitive::Variable(name))
-
-//         } else {
-
-//         }
-//     }
-
-// }
 
 #[cfg(test)]
 mod tests {
@@ -83,6 +32,6 @@ mod tests {
     #[test]
     fn instatiate_parser() {
         let string = String::from("sdasd");
-        let parser = Parser::new(&string);
+        let _ = Parser::new(&string);
     }
 }
